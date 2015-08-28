062db6917554bf8618517b5e201dc09a7b5627f9
Patrik Beyls <beylsp@yahoo>
2015-08-28 21:00:50 +0200

    adding module for user handling

---
+++ app/users.py
@@ -0,0 +1,14 @@
+from . import app
+from flask_httpauth import HTTPBasicAuth
+from flask import jsonify
+
+#extensions
+auth = HTTPBasicAuth()
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    return jsonify({'data': 'hello'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
