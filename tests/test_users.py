bd94560c172f48247dd42c3834cf4fcea6b75cf1
Patrik Beyls <beylsp@yahoo>
2015-08-28 21:42:42 +0200

    refactor test case to use generic http status code assert

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,4 +3,4 @@ from utils import AuchAppTest
 class TestUsers(AuchAppTest):
     def test_get_token_wihout_login(self):
         response = self.test_app.get('/api/token')
-        self.assertEquals(response.status, "401 UNAUTHORIZED")
+        self.assertNotAuthorized(response)
--- tests/utils.py
+++ tests/utils.py
@@ -5,3 +5,10 @@ class AuchAppTest(unittest.TestCase):
     def setUp(self):
         # create a test app every test case can use.
         self.test_app = app.test_client()
+
+    def assertStatusCode(self, response, status_code):
+        self.assertEquals(response.status_code, status_code)
+        return response
+
+    def assertNotAuthorized(self, response):
+        return self.assertStatusCode(response, 401)
