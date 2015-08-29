8c3ae3e84e630ab6907e36945333a5b324c3ebd2
Patrik Beyls <beylsp@yahoo>
2015-08-29 13:24:14 +0200

    initialize and populate test db during setUp

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,34 @@
 from utils import AuchAppTest
+import base64
 
 class TestUsers(AuchAppTest):
+    
+    def get_token(self, auth=None, **kwargs):
+        headers = kwargs.get('headers', {})
+        if auth:
+            headers['Authorization'] = 'Basic ' + base64.b64encode('%s:%s' % auth)
+
+        kwargs['headers'] = headers
+
+        return self.test_app.open('/api/token', method='get', **kwargs)
+    
     def test_get_token_wihout_login(self):
-        response = self.test_app.get('/api/token')
+        response = self.get_token()
         self.assertNotAuthorized(response)
+
+    def test_get_token_with_invalid_user(self):
+        response = self.get_token(auth=('joe', 'doe'))
+        self.assertNotAuthorized(response)
+
+    def test_get_token_with_invalid_password(self):
+        response = self.get_token(auth=('john', 'bloggs'))
+        self.assertNotAuthorized(response)
+
+    def test_get_token_with_invalid_user_and_password(self):
+        response = self.get_token(auth=('joe', 'bloggs'))
+        self.assertNotAuthorized(response)
+
+    def test_get_token_with_login(self):
+        response = self.get_token(auth=('john', 'doe'))
+        self.assertOk(response)
+        
--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,44 @@
 import unittest
+import tempfile
+import os
+
 from auchapp import app
+from auchapp.models import db
+from auchapp.models import User
 
 class AuchAppTest(unittest.TestCase):
     def setUp(self):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config.update(
+            TESTING=True,
+            SQLALCHEMY_DATABASE_URI='sqlite:////%s' % app.config['DATABASE']
+        )
+        
+        # create db and populate
+        db.create_all()
+        user = User(username='john')
+        user.hash_password(password='doe')
+        db.session.add(user)
+        db.session.commit()
+
         # create a test app every test case can use.
         self.test_app = app.test_client()
 
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
     def assertStatusCode(self, response, status_code):
+        """Assert the status code of a Flask test client response."""
         self.assertEquals(response.status_code, status_code)
         return response
 
+    def assertOk(self, response):
+        """Test that response status code is 200."""
+        return self.assertStatusCode(response, 200)
+
     def assertNotAuthorized(self, response):
+        """Test that response status code is 401."""
         return self.assertStatusCode(response, 401)
