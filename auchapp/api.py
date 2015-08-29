26839da5c4a4914565579225b40bb5e3d2f710ab
Patrik Beyls <beylsp@yahoo>
2015-08-29 13:21:53 +0200

    add verify_password() as implementation of the @login_required callback

--- auchapp/api.py
+++ auchapp/api.py
@@ -1,14 +1,26 @@
 from auchapp import app
-from flask_httpauth import HTTPBasicAuth
+from auchapp.models import User
 from flask import jsonify
+from flask import g
+from flask_httpauth import HTTPBasicAuth
 
 #extensions
 auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
 
 @app.route('/api/token')
 @auth.login_required
 def get_auth_token():
     return jsonify({'data': 'hello'})
-
-if __name__ == '__main__':
-    app.run(debug=True)
