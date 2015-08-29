7825cc6e224169e4e175134d0183aa22197d36f2
Patrik Beyls <beylsp@yahoo>
2015-08-30 00:24:14 +0200

    adding token based authentication: token generation and verification

--- auchapp/models.py
+++ auchapp/models.py
@@ -1,11 +1,15 @@
+from auchapp import app
 from auchapp import db
 from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import BadSignature
+from itsdangerous import SignatureExpired
 
 class User(db.Model):
     
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(28), index=True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(28), index = True)
     password_hash = db.Column(db.String(128))
 
     def hash_password(self, password):
@@ -14,9 +18,18 @@ class User(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
-    def generate_auth_token(self, expiration=600):
-        pass
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({'id': self.id})
     
     @staticmethod
     def verify_auth_token(token):
-        return None
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None
+        except BadSignature:
+            return None
+        user = User.query.get(data['id'])
+        return user
